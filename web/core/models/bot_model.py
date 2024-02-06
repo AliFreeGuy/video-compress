72a6aaeb6cc88edb7b672fe876742cc8700c4e34
AliFreeGuy <alifreeguy@gmail.com>
2024-02-06 11:00:36 +0330

    fix: update bot models add api hash api id field to bot model

--- web/core/models/bot_model.py
+++ web/core/models/bot_model.py
@@ -6,6 +6,8 @@ class BotModel(models.Model):
     name = models.CharField(max_length = 128 , unique = True)
     username = models.CharField(max_length = 128 , unique = True)
     token = models.CharField(max_length= 128 , unique = True)
+    api_hash = models.CharField(max_length = 225)
+    api_id = models.CharField(max_length = 225)
 
     def __str__(self) -> str:
         return str(self.name)
